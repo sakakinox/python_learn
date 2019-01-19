d364cfd0e8ac758098984364af52dd268e7ce62d
sakakinox <sakakinox@sakakinoxno-MBA.local>
2019-01-19 18:32:22 +0900

    顔認識テスト

--- cvtest.py
+++ cvtest.py
@@ -1,7 +1,20 @@
 import numpy as np
 import cv2
 
-face_cascade_path = '/usr/local/opt/opencv/share/opencv4/haarcascades/haarcascade_frontlface_default.xml'
-img_path = ''
-face_cascade = cv2.CascadeClassifire('haarcascade_frontlface_default.xml')
-img = cv2.imread('test.jpg')
+face_cascade_path = '/usr/local/opt/opencv/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
+img_path = 'test.jpg'
+result_path ='result.jpg'
+
+face_cascade = cv2.CascadeClassifier(face_cascade_path)
+img = cv2.imread(img_path)
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(img_gray)
+
+for x, y, w, h in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    face = img[y: y + h, x: x + w]
+    face_gray = img_gray[y: y + h, x: x + w]
+    
+cv2.imwrite(result_path, img)
+
