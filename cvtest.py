a976afe9952f7b4de31b2ffd1b70216135846384
sakakinox <sakakinox@sakakinoxno-MBA.local>
2019-01-19 14:40:44 +0900

    カスケードパスを導入

--- cvtest.py
+++ cvtest.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 
+face_cascade_path = '/usr/local/opt/opencv/share/opencv4/haarcascades/haarcascade_frontlface_default.xml'
+img_path = ''
 face_cascade = cv2.CascadeClassifire('haarcascade_frontlface_default.xml')
-
 img = cv2.imread('test.jpg')
